e20b802eea077209c1323b8461f366ddd6f74090
Andresh4321 <shahiandy123@gmail.com>
2026-01-20 17:53:46 +0545

    Adding endpoints

---
+++ lib/api/endpoints.ts
@@ -0,0 +1,9 @@
+//List of api routes
+//Single source of truth for api endpoints
+
+export const API={
+    AUTH:{
+        LOGIN: '/api/v1/auth/Login',
+        REGISTER: '/api/v1/auth/Register',
+    }
+}
